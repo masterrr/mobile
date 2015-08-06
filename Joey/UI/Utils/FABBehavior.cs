d16cb366c368f6c844afdb8f713969a38b5a544b
Taavi Rehemägi <taavi@toggl.com>
2015-08-06 12:05:26 +0300

    FAB and Snackbar work together.

--- Joey/UI/Utils/FABBehavior.cs
+++ Joey/UI/Utils/FABBehavior.cs
@@ -2,22 +2,23 @@
 using Android.Content;
 using Android.Support.Design.Widget;
 using Android.Support.V4.View;
-using Android.Support.V7.Widget;
 using Android.Util;
 using Android.Views;
-using Java.Interop;
-using Toggl.Joey.UI.Fragments;
-using Toggl.Joey.UI.Views;
+using Android.Widget;
 
 namespace Toggl.Joey.UI.Utils
 {
     public class FABBehavior : FloatingActionButton.Behavior
     {
         private int minMarginBottom;
+        private int shadowHeight;
+        private bool editFormCollapsed;
 
         public FABBehavior (Context context) : base ()
         {
             minMarginBottom = (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 16, context.Resources.DisplayMetrics);
+            shadowHeight = context.Resources.GetDimensionPixelSize (Resource.Dimension.ToolbarDropShadowHeight);
+            editFormCollapsed = false;
         }
 
         public FABBehavior (Context context, IAttributeSet attrs)
@@ -26,18 +27,23 @@ namespace Toggl.Joey.UI.Utils
 
         public override bool LayoutDependsOn (CoordinatorLayout parent, FloatingActionButton child, View dependency)
         {
-            return dependency.Id == Resource.Id.HomeAppBar || dependency is Snackbar;
+            return dependency.Id == Resource.Id.HomeAppBar || dependency is LinearLayout;
         }
 
         public override bool OnDependentViewChanged (CoordinatorLayout parent, FloatingActionButton child, View dependency)
         {
-            var fab = child.JavaCast<StartStopFab> ();
-            var currentInfoPaneY = ViewCompat.GetTranslationY (dependency);
-
-            var newTransY = (int)Math.Max (0, Math.Abs (dependency.GetY ()) - currentInfoPaneY - minMarginBottom - fab.Height / 2);
-            ViewCompat.SetTranslationY (fab, -newTransY);
+            if (dependency.Id == Resource.Id.HomeAppBar) {
+                float progress = (float) (dependency.Bottom - 120) / (float) (dependency.Height - 120);
+                float delta = child.Top - dependency.Height + (float) (child.Height / 2) + shadowHeight;
+                ViewCompat.SetTranslationY (child, - (delta * progress));
+            } else if (dependency.Id == Resource.Id.LogRecyclerView) {
+                if (dependency.Top > dependency.Height /5) {
+                    ViewCompat.SetTranslationY (child, - (child.Top - dependency.Top) - shadowHeight - (child.Height / 2));
+                }
+            } else if (dependency is LinearLayout && editFormCollapsed) {
+                ViewCompat.SetTranslationY (child,  dependency.TranslationY - (child.Height / 2));
+            }
             return true;
         }
-
     }
 }
