f1b812574b4000aea99baa937d64db747a564688
masterrr <masterrr1995@gmail.com>
2015-09-03 10:26:22 +0300

    LeftVC: menu stub. Left Style definition.

---
+++ Ross/Theme/Style.Left.cs
@@ -0,0 +1,17 @@
+﻿using UIKit;
+
+namespace Toggl.Ross.Theme
+{
+    public static partial class Style
+    {
+        public static class Left
+        {
+            public static void Button (UIButton v)
+            {                
+                v.SetTitleColor (UIColor.Black, UIControlState.Normal);
+                v.Font = UIFont.FromName ("HelveticaNeue", 14f);
+            }
+        }
+    }
+}
+
